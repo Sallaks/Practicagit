8911ea6da07ffd4901d4a6cb5227e49094d065ed
Manuel Gainza Alvarez <manuel.gainza@mangoodev.com>
2022-09-20 19:28:55 -0300

    "changes"

---
+++ Practicagit/Controllers/controller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practicagit.Models;
+
+namespace Practicagit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class controller : ControllerBase
+    {
+        public static List<Contact> FakeContact = new List<Contact>()
+        {
+            new Contact()
+            {   Name = "kakka",
+                CelularNumber = 12313131,
+                TelephoneNumber= 213123133123,
+                Id = 3
+
+            },
+
+             new Contact()
+             {
+
+                Name = "bababa",
+                CelularNumber = 12312311311,
+                TelephoneNumber= 2313123131,
+                Id = 4
+
+             },
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var contactoNuevo = new Contact()
+            {
+                Name = "rama1",
+                CelularNumber = 12312311311,
+                TelephoneNumber = 2313123131,
+                Id = 5
+            };
+            FakeContact.Add(contactoNuevo);
+            return Ok(FakeContact);
+        }
+
+    }
+}
+
---
+++ Practicagit/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Practicagit.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long CelularNumber { get; set; }
+        public long TelephoneNumber { get; set; }
+    }
+}
