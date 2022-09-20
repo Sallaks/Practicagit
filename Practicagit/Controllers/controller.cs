3a26fe19307121f4f136bf1baff159c9b97f661d
Geronimo Rodriguez <gero0808yt@gmail.com>
2022-09-20 19:16:06 -0300

    controlador rama 3

---
+++ Practicagit/Controllers/controller.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practicagit.Models;
+
+namespace Practicagit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class controller : ControllerBase
+    {
+        
+        
+            public static List<Contact> FakeContacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    Name = "Gero",
+                    CelularNumber = 3414121234,
+
+                },
+                new Contact()
+                {
+                    Name = "Dino",
+                    CelularNumber = 1234456789,
+
+                }
+            };
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+                return Ok(FakeContacts);
+            }
+
+            [HttpGet]
+            [Route("GetOne/{Id}")]
+            public IActionResult GetOneById(int CelularNumber)
+            {
+                return Ok(FakeContacts.Where(x => x.CelularNumber == CelularNumber).ToList());
+            }
+        
+    }
+}
+
+//Rama 3
---
+++ Practicagit/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Practicagit.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long? TelephoneNumber { get; set; }
+        public long? CelularNumber { get; set; }
+        public string? Description { get; set; }
+    }
+}
